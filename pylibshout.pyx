c8b0f69db7dd0d4a41c6801679417751384e872e
Leonardo Santos <leonardo.olsantos@gmail.com>
2014-06-12 00:19:15 -0300

    converting public to int

--- pylibshout.pyx
+++ pylibshout.pyx
@@ -385,7 +385,7 @@ cdef class Shout:
             return shout_get_public(self.shout_t)
 
         def __set__(self, public):
-            public = bool(public)
+            public = int(bool(public))
             i = shout_set_public(self.shout_t, public)
             if i != 0:
                 raise Exception(i, 'Public is not correct')
